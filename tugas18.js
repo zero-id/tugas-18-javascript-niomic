d0b9ccf474175374b3d1a1d190c1f1736a481006
zero-id <abdullatiffebriliana15@gmail.com>
2023-03-13 17:54:07 +0700

    tugas 18 js

---
+++ tugas18.js
@@ -0,0 +1,23 @@
+console.log('No. 1:');
+function panggilKata() {
+    const kalimat = 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.';
+    
+    console.log(kalimat.slice(8, 24));
+}
+
+panggilKata();
+
+console.log();
+console.log('============');
+console.log();
+
+console.log('No. 2:');
+function gabungKata() {
+    const str1 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+
+    const str2 = "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.";
+
+    console.log(str1.concat(str2));
+}
+
+gabungKata();
